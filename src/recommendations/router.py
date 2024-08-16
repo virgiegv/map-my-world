7498fcd0aec75ab40c429cd205bce1b1ee1e70fd
virgiegv <vgil_22@hotmail.com>
2024-08-15 22:13:49 -0500

    changes in requirements.txt and recommendations router

--- src/recommendations/router.py
+++ src/recommendations/router.py
@@ -1,10 +1,12 @@
 from fastapi import Depends, HTTPException, Query, APIRouter, Response
 from sqlalchemy.orm import Session
 from . import recommendation
-
+from . import schemas
 from src.db.database import get_db
 
 router = APIRouter()
 
 
-#@router.get("/recommend", tags="recommendations", )
+@router.get("/recommend", tags=["recommendations"], response_model=list[schemas.Recommendation])
+def recommend(db: Session = Depends(get_db)):
+    return recommendation.get_recommendations(db)
